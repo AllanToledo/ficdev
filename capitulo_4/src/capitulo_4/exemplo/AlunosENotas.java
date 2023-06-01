42608887aa94025f9729db78a4d2ab9c93a88332
Allan Toledo <a.llantoledo@hotmail.com>
2023-06-01 10:47:28 -0400

    Alta Floresta

---
+++ capitulo_4/src/capitulo_4/exemplo/AlunosENotas.java
@@ -0,0 +1,46 @@
+package capitulo_4.exemplo;
+
+//@author Allan Toledo
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class AlunosENotas {
+    public static class Aluno {
+        public String nome;
+        public int idade;
+        public float nota;
+
+        public Aluno(String nome, int idade, float nota) {
+            this.nome = nome;
+            this.idade = idade;
+            this.nota = nota;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + nome + " " + idade + " anos nota: " + nota + ")";
+        }
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        List<Aluno> alunos = new ArrayList();
+        
+        while(true){
+            int opcao = JOptionPane.showConfirmDialog(null, "Deseja cadastrar mais um aluno? (s|n)");
+            if(opcao != JOptionPane.YES_OPTION) break;
+            String[] entrada = JOptionPane
+                    .showInputDialog("Digite o nome, idade e nota, separadas por um espaço.").split(" ");
+            
+            alunos.add(new Aluno(entrada[0], 
+                    Integer.parseInt(entrada[1]), 
+                    Float.parseFloat(entrada[2])));
+        }
+        
+        JOptionPane.showMessageDialog(null, alunos);
+        
+    }
+}
